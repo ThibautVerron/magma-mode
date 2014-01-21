7693475d9ffce961c11af66fe6cbb49961a0de2f
Thibaut Verron <thibaut.verron@gmail.com>
2014-01-21 20:43:20 +0100

    Completion documentation

--- magma-completion.el
+++ magma-completion.el
@@ -1,10 +1,29 @@
+;; Completion in the magma buffer.
+;;
+;; No customization is available for now. The completion engine
+;; depends on an index file it expects to find in
+;; <path_to_magma-mode>/data/magma_symbols.txt, and this file should
+;; contain completion candidates, one per line. It is not clear
+;; whether it is legally possible to distribute such a file, but in
+;; the future, we will at least provide a script to build it from the
+;; magma documentation files.
+
+
 (defvar magma-completion-table-file (f-join magma-path "data/magma_symbols.txt"))
 
-(defconst magma-completion-table-base
+(defun magma-scan-index (file)
+  (interactive)
   (with-temp-buffer
-    (insert-file-contents magma-completion-table-file)
-      (split-string (buffer-string) "\n" t)))
+    (condition-case nil
+        (insert-file-contents file)
+      (error (message "The index file does not exist, so I cannot enable completion. Please see the comments to build it.")))
+    (split-string (buffer-string) "\n" t)))
 
+
+(defvar magma-completion-table-base
+  (magma-scan-index magma-completion-table-file))
+  
+  
 (defun magma-completion-at-point ()
   (let* ((bounds (bounds-of-thing-at-point 'word))
          (start (car bounds))
