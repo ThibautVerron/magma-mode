122e6e7292c22628c3b02df944111566483dd45b
Thibaut Verron <thibaut.verron@gmail.com>
2014-01-26 12:14:03 +0100

    Fixed some typos and non-assigned names

--- features/support/env.el
+++ features/support/env.el
@@ -25,7 +25,7 @@
  (setq magma-interactive-program
        (let ((ext (if (eq system-type 'windows-nt) ".bat" ".sh")))
        (f-join magma-mode-root-path "bin" (s-concat "dummymagma" ext))))
- (setq magma-completion-table-file (j-join magma-path
+ (setq magma-completion-table-file (f-join magma-path
                                            "data/dummymagma_symbols.txt"))
 )
 
--- test/test-helper.el
+++ test/test-helper.el
@@ -4,8 +4,8 @@
 
 (setq magma-interactive-program
       (let ((ext (if (eq system-type 'windows-nt) ".bat" ".sh")))
-        (f-join magma-mode-root-path "bin" (s-concat "dummymagma" ext))))
-(setq magma-completion-table-file (j-join magma-path
+        (f-join magma-path "bin" (s-concat "dummymagma" ext))))
+(setq magma-completion-table-file (f-join magma-path
                                           "data/dummymagma_symbols.txt"))
 
 
