1e61ad9f6a9500f3e5d27c0f0e54303fc8eff128
Thibaut Verron <thibaut.verron@gmail.com>
2014-01-26 11:05:04 +0100

    Use the shorter files for the tests

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,5 +2,11 @@
 
 (require 'magma-mode)
 
+(setq magma-interactive-program
+      (let ((ext (if (eq system-type 'windows-nt) ".bat" ".sh")))
+        (f-join magma-mode-root-path "bin" (s-concat "dummymagma" ext))))
+(setq magma-completion-table-file (j-join magma-path
+                                          "data/dummymagma_symbols.txt"))
+
 
 
