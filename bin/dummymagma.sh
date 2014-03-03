057cce51e9a20af5dc9cb063a3e85f9bed188545
Thibaut Verron <thibaut.verron@gmail.com>
2014-03-03 10:29:50 +0100

    Errors and documentation

--- bin/dummymagma.sh
+++ bin/dummymagma.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 echo "Welcome to dummymagma v1.0!"
+echo "(Type \"help\" for help)"
 
 while true; do
     read -p "> " line
     echo "> $line" # This line should be deleted by comint
     case "$line" in
+        "help")
+            echo "Commands:"
+            echo "- error: output an error"
+            echo "- hang: the process will hang until killed"
+            echo "- help: display this"
+            echo "Anything else is just reechoed."
+            ;;
         "hang")
             while true ; do :; done
+            ;;
+        "error")
+            echo "In file file.m, line 1, column 1:"
+            echo "Error"
             ;;
         *)
             echo "Input: $line" # This line should appear
